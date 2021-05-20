4ea7b8840f169695bf28ad572313473126681b2b
BIT <BIT@DESKTOP-DSRR2KB>
2021-05-20 17:19:32 +0900

    Delete - stmt

--- src/main/java/test/DeleteTest01.java
+++ src/main/java/test/DeleteTest01.java
@@ -2,60 +2,58 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DeleteTest01 {
 
 	public static void main(String[] args) {
-		Boolean result = delete(5L);
-
-		System.out.println(result ? "성공!" : "실패");
+		Boolean result = delete(2L);
 	}
 
 	public static boolean delete(Long no) {
 
-		// TODO Auto-generated method stub
 		Connection conn = null;
 		Statement stmt = null;
 		boolean result = false;
 
 		try {
-			// 1. JDBC Driver 로딩
+			// 1. 드라이버 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			// 2. Driver Manager 커넥션 얻기
-			String url = "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
+			// 2. Connection 객체 연결
+			String url = "jdbc:mysql://192.168.80.131:3307/employees?charset=utf8";
 			conn = DriverManager.getConnection(url, "hr", "hr");
 
-			// 3. statement 생성
+			// 3. stmt 생성
 			stmt = conn.createStatement();
 
-			// 4. SQL 문 실행
-			String sql = "delete" + " from dept" + " where no= " + no;
+			// 4. SQL문 생성
+			String sql = "delete" + "  from dept" + " where no = " + no;
 
 			int count = stmt.executeUpdate(sql);
 			result = count == 1;
 
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 설치 실패: " + e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("error" + e);
+			System.out.println("error:" + e);
 		} finally {
 			try {
-				// 자원정리
+				// 자원정리(clean-up)
 				if (stmt != null) {
 					stmt.close();
 				}
 				if (conn != null) {
 					conn.close();
 				}
-
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+
 		return result;
 
 	}
