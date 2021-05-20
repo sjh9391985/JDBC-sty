4c68c298cc4520887318e8e14de8817304763e0e
BIT <BIT@DESKTOP-DSRR2KB>
2021-05-20 17:08:31 +0900

    Select - stmt 연습

--- src/main/java/test/SelectTest.java
+++ src/main/java/test/SelectTest.java
@@ -9,57 +9,55 @@ import java.sql.Statement;
 public class SelectTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-
+		
 		try {
-			// 1. JDBC Driver 로딩
+			//1. JDBC Driver 로드
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			// 2. Driver Manager 커넥션 얻기
+			//2. Connecton 객체 얻기
 			String url = "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
 			conn = DriverManager.getConnection(url, "hr", "hr");
-
-			// 3. statement 생성
+			
+			//3. stmt 생성
 			stmt = conn.createStatement();
-
-			// 4. SQL 문 실행
-			String sql = "select emp_no, birth_date, last_name" + " from employees" + " where first_name like 'pat%' ";
+			
+			//4. SQL 문 실행
+			String sql = "select emp_no, birth_date, last_name from employees where first_name = 'pat%' ";
 			rs = stmt.executeQuery(sql);
-
-			// 5. 결과 가져오기
-			while (rs.next()) {
-				Long empNO = rs.getLong(1);
+			
+			while(rs.next()) {
+				Long empNo = rs.getLong(1);
 				String birthDate = rs.getString(2);
 				String lastName = rs.getString(3);
-
-				System.out.println(empNO + " : " + birthDate + " : " + lastName);
 			}
-
+			
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 설치 실패: " + e);
+			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
-			System.out.println("error" + e);
+			System.out.println("error:" + e);
 		} finally {
 			try {
-				// 자원정리
-				if (rs != null) {
+				// 자원정리(clean-up)
+				if(rs != null) {
 					rs.close();
 				}
-				if (stmt != null) {
+				if(stmt != null) {
 					stmt.close();
 				}
-				if (conn != null) {
+				if(conn != null) {
 					conn.close();
 				}
-
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		
+		
+		
 	}
 
 }
