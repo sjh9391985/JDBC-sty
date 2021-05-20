264952650cf903d2e2c38e47d6f2c26b1969b63e
BIT <BIT@DESKTOP-DSRR2KB>
2021-05-20 11:00:51 +0900

    JDBC - select, insert

---
+++ src/main/java/test/ConnectionTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// 2. Driver Manager 커넥션 얻기
+			String url = "jdbc:mysql://192.168.80.114:3307/webdb>charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+
+			// 3. 연결 성공
+			System.out.println("OK: " + conn);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 설치 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error" + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/main/java/test/InsertTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTest {
+	public static void main(String[] args) {
+		insertDepartment("홍보");
+		
+	}
+	
+	public static boolean insertDepartment(String name) {
+		
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement stmt = null;
+		boolean result = false;
+		
+
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// 2. Driver Manager 커넥션 얻기
+			String url = "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+
+			// 3. statement 생성
+			stmt = conn.createStatement();
+
+			// 4. SQL 문 실행
+			String sql = "insert" + " into dept" + " values (null,'"+name+"')";
+			
+			int count = stmt.executeUpdate(sql);
+			result = count ==1;
+			
+
+			
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 설치 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error" + e);
+		} finally {
+			try {
+				// 자원정리
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+}
