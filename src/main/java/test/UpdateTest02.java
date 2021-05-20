58fffefd195c4b6bca62b3484d9bc3c8d0c621ca
BIT <BIT@DESKTOP-DSRR2KB>
2021-05-20 18:37:19 +0900

    update - pstmt

--- src/main/java/test/UpdateTest02.java
+++ src/main/java/test/UpdateTest02.java
@@ -8,61 +8,52 @@ import java.sql.Statement;
 
 public class UpdateTest02 {
 	public static void main(String[] args) {
-		DeptVo vo = new DeptVo();
-		vo.setNo(4L);
-		vo.setName("전략실");
-		
-		Boolean result = update(vo);
-		System.out.println(result ? "성공!" : "실패");
-	}
-	
-	public static Boolean update(DeptVo vo) {
-		
 
-		// TODO Auto-generated method stub
-		Connection conn = null;		
+	}
+
+	public static Boolean update(DeptVo vo) {
+		Connection conn = null;
 		PreparedStatement pstmt = null;
 		boolean result = false;
-
 		try {
-			// 1. JDBC Driver 로딩
+			// 1. driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			// 2. Driver Manager 커넥션 얻기
+			// 2. connection 객체 생성
 			String url = "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
 			conn = DriverManager.getConnection(url, "hr", "hr");
 
-			// 3. SQL문 준비
-			String sql = "update dept" + " set name =  ?   " + "where no = ?";
+			// 3. SQL 준비
+			String sql = "";
 			pstmt = conn.prepareStatement(sql);
 
-			// 4. binding
+			// 4. 바인딩
 			pstmt.setString(1, vo.getName());
-			pstmt.setLong(2,vo.getNo());
-			
-			// 5. SQL문 실행
+			pstmt.setLong(2, vo.getNo());
+
+			// 5. SQL 실행
 			int count = pstmt.executeUpdate();
 			result = count == 1;
 
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 설치 실패: " + e);
+			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
-			System.out.println("error" + e);
+			System.out.println("error:" + e);
 		} finally {
 			try {
-				// 자원정리
+				// 자원정리(clean-up)
 				if (pstmt != null) {
 					pstmt.close();
 				}
 				if (conn != null) {
 					conn.close();
 				}
-
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+
 		return result;
 	}
+
 }
