46a8853b2d13867aadaf4c2c645b7c8854f83e46
BIT <BIT@DESKTOP-DSRR2KB>
2021-05-20 17:42:59 +0900

    delete -pstmt

--- src/main/java/test/DeleteTest02.java
+++ src/main/java/test/DeleteTest02.java
@@ -8,57 +8,59 @@ import java.sql.Statement;
 
 public class DeleteTest02 {
 	public static void main(String[] args) {
-		Boolean result = delete(4L);
-
-		System.out.println(result ? "성공!" : "실패");
-	}
+		delete(1L);	
+		}
 
 	public static boolean delete(Long no) {
-
-		// TODO Auto-generated method stub
+		
 		Connection conn = null;
-		boolean result = false;
 		PreparedStatement pstmt = null;
-
+		Boolean result = false;
 		try {
-			// 1. JDBC Driver 로딩
+			//1. Driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
-
-			// 2. Driver Manager 커넥션 얻기
-			String url = "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
-			conn = DriverManager.getConnection(url, "hr", "hr");
-
-			// 3. SQL 문 실행
-			String sql = "delete" + " from dept" + " where no= ?" ;
-			pstmt = conn.prepareStatement(sql);
 				
-			// 4. 바인딩
-			pstmt.setLong(1, no);
-
+			//2. connection 연결
+			String url =  "jdbc:mysql://192.168.80.114:3307/employees?charset=utf8";
+			conn = DriverManager.getConnection(url,"hr","hr");
 			
+			//3. SQL 준비
+			String sql =
+					"delete" +
+						    "  from dept" + 
+							" where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. binding
+			pstmt.setLong(1, no);
+			
+			//5. SQL 실행
 			int count = pstmt.executeUpdate();
-			result = count == 1;
-
+			result = count == 1; 
+			
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 설치 실패: " + e);
+			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
-			System.out.println("error" + e);
-		} finally {
+			System.out.println("error:" + e);
+		}finally {
 			try {
-				// 자원정리
-				if (pstmt != null) {
+				// 자원정리(clean-up)
+				if(pstmt != null) {
 					pstmt.close();
 				}
-				if (conn != null) {
+				if(conn != null) {
 					conn.close();
 				}
-
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		return result;
 
+		return result;
+		
+		
+		
+		
 	}
+
 }
